670951502d3c7253f12d3ebc350fbe6ac4f987f1
LyhourChhen <ukamilove66@gmail.com>
2021-03-14 10:17:36 +0700

    update more rule to eslint to upgrade to version 1.0.1

--- src/eslint.ts
+++ src/eslint.ts
@@ -1,36 +1,42 @@
-import * as path from 'path';
-import * as fs from 'fs';
-import tsEslintConfig from './tsEslintConfig';
+import * as path from "path";
+import * as fs from "fs";
+import tsEslintConfig from "./tsEslintConfig";
 
 const parserOptions = {
   ecmaFeatures: {
     jsx: true,
   },
   babelOptions: {
-    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+    presets: [
+      "@babel/preset-env",
+      "@babel/preset-react",
+      "@babel/preset-typescript",
+    ],
     plugins: [
-      ['@babel/plugin-proposal-decorators', { legacy: true }],
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
     ],
   },
   requireConfigFile: false,
-  project: './tsconfig.json',
+  project: "./tsconfig.json",
 };
 
-const isJsMoreTs = async (path = 'src') => {
-  const fg = require('fast-glob');
+const isJsMoreTs = async (path = "src") => {
+  const fg = require("fast-glob");
   const jsFiles = await fg(`${path}/src/**/*.{js,jsx}`, { deep: 3 });
   const tsFiles = await fg(`${path}/src/**/*.{ts,tsx}`, { deep: 3 });
   return jsFiles.length > tsFiles.length;
 };
 
-const isTsProject = fs.existsSync(path.join(process.cwd() || '.', './tsconfig.json'));
+const isTsProject = fs.existsSync(
+  path.join(process.cwd() || ".", "./tsconfig.json")
+);
 
 if (isTsProject) {
   try {
     isJsMoreTs(process.cwd()).then((jsMoreTs) => {
       if (!jsMoreTs) return;
-      console.log('For TS with tsconfig.json');
+      console.log("For TS with tsconfig.json");
     });
   } catch (e) {
     console.log(e);
@@ -38,13 +44,13 @@ if (isTsProject) {
 }
 
 module.exports = {
-  extends: ['eslint-config-airbnb-base', 'prettier', 'prettier/react'].concat(
+  extends: ["eslint-config-airbnb-base", "prettier"].concat(
     isTsProject
-      ? ['prettier/@typescript-eslint', 'plugin:@typescript-eslint/recommended']
-      : ['plugin:react/recommended'],
+      ? ["prettier/@typescript-eslint", "plugin:@typescript-eslint/recommended"]
+      : ["plugin:react/recommended"]
   ),
-  parser: isTsProject ? '@typescript-eslint/parser' : '@babel/eslint-parser',
-  plugins: ['eslint-comments', 'react', 'jest', 'unicorn', 'react-hooks'],
+  parser: isTsProject ? "@typescript-eslint/parser" : "@babel/eslint-parser",
+  plugins: ["eslint-comments", "react", "jest", "unicorn", "react-hooks"],
   env: {
     browser: true,
     node: true,
@@ -54,74 +60,76 @@ module.exports = {
     jasmine: true,
   },
   rules: {
-    'react/display-name': 0,
-    'react/jsx-props-no-spreading': 0,
-    'react/state-in-constructor': 0,
-    'react/static-property-placement': 0,
+    "react/display-name": 0,
+    "react/jsx-props-no-spreading": 0,
+    "react/state-in-constructor": 0,
+    "react/static-property-placement": 0,
     // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
-    'react/destructuring-assignment': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/no-array-index-key': 'warn',
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-    'react-hooks/exhaustive-deps': 'warn', // Checks deps of Hooks
-    'react/require-default-props': 0,
-    'react/jsx-fragments': 0,
-    'react/jsx-wrap-multilines': 0,
-    'react/prop-types': 0,
-    'react/forbid-prop-types': 0,
-    'react/sort-comp': 0,
-    'react/react-in-jsx-scope': 0,
-    'react/jsx-one-expression-per-line': 0,
-    'generator-star-spacing': 0,
-    'function-paren-newline': 0,
-    'import/no-unresolved': 0,
-    'import/order': 0,
-    'import/no-named-as-default': 0,
-    'import/no-cycle': 0,
-    'import/prefer-default-export': 0,
-    'import/no-default-export': 0,
-    'import/no-extraneous-dependencies': 0,
-    'import/named': 0,
-    'import/no-named-as-default-member': 0,
-    'import/no-duplicates': 0,
-    'import/no-self-import': 0,
-    'import/extensions': 0,
-    'import/no-useless-path-segments': 0,
-    'jsx-a11y/no-noninteractive-element-interactions': 0,
-    'jsx-a11y/click-events-have-key-events': 0,
-    'jsx-a11y/no-static-element-interactions': 0,
-    'jsx-a11y/anchor-is-valid': 0,
-    'sort-imports': 0,
-    'class-methods-use-this': 0,
-    'no-confusing-arrow': 0,
-    'linebreak-style': 0,
+    "react/destructuring-assignment": "off",
+    "react/jsx-filename-extension": "off",
+    "react/no-array-index-key": "warn",
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn", // Checks deps of Hooks
+    "react/require-default-props": 0,
+    "react/jsx-fragments": 0,
+    "react/jsx-wrap-multilines": 0,
+    "react/prop-types": 0,
+    "react/forbid-prop-types": 0,
+    "react/sort-comp": 0,
+    "react/react-in-jsx-scope": 0,
+    "react/jsx-one-expression-per-line": 0,
+    "generator-star-spacing": 0,
+    "function-paren-newline": 0,
+    "import/no-unresolved": 0,
+    "import/order": 0,
+    "import/no-named-as-default": 0,
+    "import/no-cycle": 0,
+    "import/prefer-default-export": 0,
+    "import/no-default-export": 0,
+    "import/no-extraneous-dependencies": 0,
+    "import/named": 0,
+    "import/no-named-as-default-member": 0,
+    "import/no-duplicates": 0,
+    "import/no-self-import": 0,
+    "import/extensions": 0,
+    "import/no-useless-path-segments": 0,
+    "jsx-a11y/no-noninteractive-element-interactions": 0,
+    "jsx-a11y/click-events-have-key-events": 0,
+    "jsx-a11y/no-static-element-interactions": 0,
+    "jsx-a11y/anchor-is-valid": 0,
+    "sort-imports": 0,
+    "class-methods-use-this": 0,
+    "no-confusing-arrow": 0,
+    "linebreak-style": 0,
     // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
-    'no-prototype-builtins': 'off',
-    'unicorn/prevent-abbreviations': 'off',
+    "no-prototype-builtins": "off",
+    "unicorn/prevent-abbreviations": "off",
     // Conflict with prettier
-    'arrow-body-style': 0,
-    'arrow-parens': 0,
-    'object-curly-newline': 0,
-    'implicit-arrow-linebreak': 0,
-    'operator-linebreak': 0,
-    'eslint-comments/no-unlimited-disable': 0,
-    'no-param-reassign': 2,
-    'space-before-function-paren': 0,
+    "arrow-body-style": 0,
+    "arrow-parens": 0,
+    "object-curly-newline": 0,
+    "implicit-arrow-linebreak": 0,
+    "operator-linebreak": 0,
+    "eslint-comments/no-unlimited-disable": 0,
+    "no-param-reassign": 2,
+    "space-before-function-paren": 0,
     ...(isTsProject ? tsEslintConfig : {}),
   },
   settings: {
     // support import modules from TypeScript files in JavaScript files
-    'import/resolver': {
+    "import/resolver": {
       node: {
-        extensions: isTsProject ? ['.js', '.jsx', '.ts', '.tsx', '.d.ts'] : ['.js', '.jsx'],
+        extensions: isTsProject
+          ? [".js", ".jsx", ".ts", ".tsx", ".d.ts"]
+          : [".js", ".jsx"],
       },
     },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx", ".d.ts"],
     },
-    'import/extensions': ['.js', '.mjs', '.jsx', '.ts', '.tsx', '.d.ts'],
-    'import/external-module-folders': ['node_modules', 'node_modules/@types'],
-    polyfills: ['fetch', 'Promise', 'URL', 'object-assign'],
+    "import/extensions": [".js", ".mjs", ".jsx", ".ts", ".tsx", ".d.ts"],
+    "import/external-module-folders": ["node_modules", "node_modules/@types"],
+    polyfills: ["fetch", "Promise", "URL", "object-assign"],
   },
   parserOptions,
 };
